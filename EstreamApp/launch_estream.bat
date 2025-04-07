6fb53182543bda0e3b6b90a9aa0abe3d29e9b9f9
Mikhail_Shell <theoutstandingme@gmail.com>
2025-04-07 16:29:38 +0700

    Now multiple connections are handled

---
+++ EstreamApp/launch_estream.bat
@@ -0,0 +1,3 @@
+start "Estream" cmd /k "title Encryptor_1 && ../hc-128/x86/Debug/hc128.exe 8000"
+start "Estream" cmd /k "title Encryptor_2 && ../hc-128/x86/Debug/hc128.exe 9000"
+start "Estream" cmd /k "title Server && ../../"
